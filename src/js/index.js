fa5c9afa352cba93d293221fe95451505463ede5
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-05 18:04:44 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,8 @@ import './../css/styles.css';
 const form = document.querySelector("#search-form");
 console.dir(form);
 
-// form.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
