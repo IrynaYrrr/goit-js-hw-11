34947a9cbb0fd2e9334bb2a5b55cdaac9a25281c
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-06 01:05:25 +0300

    update

--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -1,5 +1,5 @@
-export async function fetchImage(searchValue) {
-  const response = await fetch(`https://pixabay.com/api/?key=36088783-799e53020b824ac98b477fb5a&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`);
+export async function fetchImage(searchValue, page) {
+  const response = await fetch(`https://pixabay.com/api/?key=36088783-799e53020b824ac98b477fb5a&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
   const images = await response.json();
   return images;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,50 @@
 import './../css/styles.css';
 import { fetchImage } from './fetchImage';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+let page = 1;
 const form = document.querySelector("#search-form");
 const input = document.querySelector("input");
 const gallery = document.querySelector(".gallery");
+const buttonLoadMore = document.querySelector("button[type=button]");
+
+buttonLoadMore.style.display="none";
+
+// console.log(buttonLoadMore);
+
+buttonLoadMore.addEventListener('click', onClick);
 
 form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(e) {
-  e.preventDefault();
+
+async function searchFn() {
+  buttonLoadMore.style.display="none";
   const inputValue = input.value.trim();
-  const images = await fetchImage(inputValue);
+  const images = await fetchImage(inputValue, page);
+
+  console.log(images);
+
+  if(images.hits.length === 0){
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.',  {
+      width: '300px',
+      position: 'center-center',
+      distance: '10px',
+    });
+
+    return;
+  }
+
+  if(images.hits.length < 40) {
+    Notify.info("We're sorry, but you've reached the end of search results.",  {
+      width: '300px',
+      position: 'center-bottom',
+      distance: '10px',
+    });
+  }
+  else {
+    buttonLoadMore.style.display="block";
+  }
+
   renderImgList(images.hits);
 }
 
@@ -37,4 +71,16 @@ function renderImgList(images){
       })
       .join("");
       gallery.innerHTML = markup;
+};
+
+function onClick(e) {
+  e.preventDefault();
+  page += 1;
+  searchFn();
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  page = 1;
+  searchFn();
 }
