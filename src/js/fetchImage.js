d12d72dce30610f8788a3279261967c1c83c0aac
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-07 17:36:52 +0300

    add axios

--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -1,5 +1,7 @@
+import axios from "axios";
+
 export async function fetchImage(searchValue, page) {
-  const response = await fetch(`https://pixabay.com/api/?key=36088783-799e53020b824ac98b477fb5a&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-  const images = await response.json();
+  const response = await axios.get(`https://pixabay.com/api/?key=36088783-799e53020b824ac98b477fb5a&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+  const images = response.data;
   return images;
 }
